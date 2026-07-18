0ff6d450363de84c086730714e0e5813e4e7858d
Kostas Papakonstantinou <kostas@bitsikas.dev>
2026-07-18 22:30:51 +0300

    fix writing

--- src/smtping/server.py
+++ src/smtping/server.py
@@ -51,9 +51,8 @@ class PongHandler:
     async def handle_DATA(self, server, session, envelope):
 
         tmp_path = "/tmp/smtping/"
-        tmpfile = tempfile.NameTemporaryFile(dir=tmp_path, delete=False).name
-        with open(tmpfile, "wb") as f:
-            f.write(envelope.content)
+        tmpfile = tempfile.NamedTemporaryFile(dir=tmp_path, delete=False).name
+        tmpfile.write(envelope.content)
         return "250 OK"
 
 
